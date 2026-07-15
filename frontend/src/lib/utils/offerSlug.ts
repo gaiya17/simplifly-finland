e7de428b526c2f7cc2cb173ff102a99028a38543
gaiya17 <nirmalgayantha55@gmail.com>
2026-07-15 09:50:25 +0530

    Fix syntax error in offerSlug.ts

---
+++ frontend/src/lib/utils/offerSlug.ts
@@ -0,0 +1,7 @@
+export const generateOfferSlug = (offer: any): string => {
+  if (!offer) return 'special-offer';
+  if (offer.nights) {
+    return `${offer.nights}-nights-offer`;
+  }
+  return 'special-offer';
+};
